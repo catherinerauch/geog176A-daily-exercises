a0424aae0b6369608a268a36ef9ba0f8a3153ccc
catherinerauch <69225124+catherinerauch@users.noreply.github.com>
2020-08-19 12:46:38 -0700

    day 8 assignment

--- R/day-08.R
+++ R/day-08.R
@@ -1,31 +1,22 @@
 # Catherine Rauch
-# 8/14/2020
-# daily assignment 8
+# 8/18/20
 
-url <- 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
-covid <-  read_csv(url)
+CONUS <- USAboundaries::us_states() %>%
+          filter(!state_name %in% c( "Puerto Rico", "Alaska","Hawaii"))
 
-head(covid)
-
-state.of.interest = "Washington"
-
-WAcovidnewcases <- covid %>%
-  filter(state == state.of.interest) %>%  # filter data for cases in Washington state only
-  group_by(date) %>%
-  summarise(ncases = sum(cases), .groups = 'drop') %>%
-  mutate(newCases = ncases - lag(ncases),
-         roll7 = rollmean(newCases, 7, fill = NA, align="right")) %>%
-  ggplot(aes(x = date)) +
-  geom_col(aes(y = newCases), col = NA, fill = "#F5B8B5") +
-  geom_line(aes(y = roll7), col = "darkred", size = 1) +
-  ggthemes::theme_wsj() +
-  labs(title = paste("New Reported cases by day in", state.of.interest)) +
-  theme(plot.background = element_rect(fill = "white"),
-        panel.background = element_rect(fill = "white"),
-        plot.title = element_text(size = 14, face = 'bold')) +
-  theme(aspect.ratio = .5)
+# cast CONUS into a MULTILINESTRING Geometry set of 1 feature
 
 
-png("WAcovidnewcases.png")
-print(WAcovidnewcases)
-dev.off()
+# preserve internal state boundaries
+sboundariesCONUS.ml <-  st_combine(CONUS) %>%
+                      st_cast("MULTILINESTRING")
+
+sboundariesCONUS.ml
+plot(sboundariesCONUS.ml)
+
+
+# dissolve internal state boundaries
+nboundariesCONUS.ml = st_union(CONUS) %>%
+                      st_cast("MULTILINESTRING")
+nboundariesCONUS.ml
+plot(nboundariesCONUS.ml)
