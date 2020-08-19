ffde841d1be2609fba71711d6a3a2539b34d1afa
catherinerauch <69225124+catherinerauch@users.noreply.github.com>
2020-08-19 12:53:13 -0700

    fixed error with day 8 and day 10 assignments

---
+++ R/day-10.R
@@ -0,0 +1,23 @@
+# Catherine Rauch
+# 8/18/20
+# daily assignment 10
+
+CONUS <- USAboundaries::us_states() %>%
+  filter(!state_name %in% c( "Puerto Rico", "Alaska","Hawaii"))
+
+# cast CONUS into a MULTILINESTRING Geometry set of 1 feature
+
+
+# preserve internal state boundaries
+sboundariesCONUS.ml <-  st_combine(CONUS) %>%
+  st_cast("MULTILINESTRING")
+
+sboundariesCONUS.ml
+plot(sboundariesCONUS.ml)
+
+
+# dissolve internal state boundaries
+nboundariesCONUS.ml = st_union(CONUS) %>%
+  st_cast("MULTILINESTRING")
+nboundariesCONUS.ml
+plot(nboundariesCONUS.ml)
